1ed34e6bec514dcb902018499a8b3664d65b0a03
Marcus Barnes <mavjbarnes@gmail.com>
2015-09-05 23:23:35 -0400

    added globals

---
+++ app.js
@@ -0,0 +1,21 @@
+// Globals
+var newTaskInput = document.getElementById('new-task'),
+    addButton = document.getElementsByTagName('button')[0],
+    incompleteTaskHolder = document.getElementById('incomplete-tasks'),
+    completedTaskHolder = document.getElementById('completed-taskss');
+
+var addTask = function() {
+  // body...
+};
+
+var editTask = function() {
+  // body...
+};
+
+var completedTask = function() {
+  // body...
+};
+
+var incompleteTask = function() {
+  // body...
+};
